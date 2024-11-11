d72b7a4c81987a78051240eb80ccf054e14623c2
Nathan Rodriguez <nathan94_roro@hotmail.fr>
2024-11-11 16:54:36 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import Home from './components/Home'
+
+function App() {
+  return (
+    <div className="App">
+      <Home />
+      <Home />
+
+      <Home /><Home />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Home.js
@@ -0,0 +1,9 @@
+import '../style/home.css'
+function Home() {
+    return (
+        <div className="home">Bonjour je suis la Home.
+        </div>
+    );
+}
+
+export default Home;
