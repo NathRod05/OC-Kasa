164415e928a4562a9011fcf4ab1d6194ec44fca3
Nathan Rodriguez <nathan94_roro@hotmail.fr>
2024-11-21 20:36:45 -0500

    Ajout className cards

--- src/Page/Home.js
+++ src/Page/Home.js
@@ -2,14 +2,17 @@ import '../style/style.css'
 import Banner from '../components/Banner'
 import Card from '../components/Card'
 import data from '../data.json'
+import '../style/card.css'
 
 function Home() {
   return (
     <div className="style">
       <Banner />
-      {data.map((card) => {
-        return <Card key={card.id} title={card.title} cover={card.cover} />
-      })}
+      <div className="cards">
+        {data.map((card) => {
+          return <Card key={card.id} title={card.title} cover={card.cover} />
+        })}
+      </div>
     </div>
   )
 }
