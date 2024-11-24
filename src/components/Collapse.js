fff5d70ce7adf9363bb3b6ef569cc75a99ff5fdf
Nathan Rodriguez <nathan94_roro@hotmail.fr>
2024-11-23 21:18:47 -0500

    Ajout useState pour Collapse

--- src/components/Collapse.js
+++ src/components/Collapse.js
@@ -2,12 +2,13 @@ import React, { useState } from 'react'
 import '../style/collapse.css'
 
 function Collapse({ title, description }) {
+  const [open, setOpen] = useState(false)
   return (
-    <div className="collapse">
+    <div className="collapse" onClick={() => setOpen(!open)}>
       <button className="collapse__btn">
         {title} <i class="fa-solid fa-chevron-up"></i>
       </button>
-      <ul className="descritpion">{description}</ul>
+      {open ? <ul className="descritpion">{description}</ul> : ''}
     </div>
   )
 }
