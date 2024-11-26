3bf8200d6b25d9bd03f26246346ce87debe4d8af
Nathan Rodriguez <nathan94_roro@hotmail.fr>
2024-11-26 16:59:19 -0500

    Ajout carrousel au fichier Accommodation

--- src/Page/Accommodation.js
+++ src/Page/Accommodation.js
@@ -1,35 +1,13 @@
 import '../style/style.css'
 import data from '../data.json'
 import Carrousel from '../components/Carrousel'
-import InfoProperty from '../components/InfoProperty'
+// import InfoProperty from '../components/InfoProperty'
 
 function Accommodation() {
   return (
     <div className="page">
       {data.map((carrousel) => {
-        return (
-          <Carrousel
-            key={carrousel}
-            id={carrousel.id}
-            pictures={carrousel.pictures}
-          />
-        )
-      })}
-      {data.map((property) => {
-        return (
-          <InfoProperty
-            key={property}
-            id={property.id}
-            title={property.title}
-            location={property.location}
-            name={property.name}
-            picture={property.pictures}
-            tags={property.tags}
-            rating={property.rating}
-            description={property.description}
-            equipments={property.equipments}
-          />
-        )
+        return <Carrousel key={carrousel.id} pictures={carrousel.pictures} />
       })}
     </div>
   )
