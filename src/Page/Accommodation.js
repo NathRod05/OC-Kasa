a90b60360b17d3b5e5508881a49ba349ca4ac814
Nathan Rodriguez <nathan94_roro@hotmail.fr>
2024-11-27 01:53:05 -0500

    Ajout import infoproperty

--- src/Page/Accommodation.js
+++ src/Page/Accommodation.js
@@ -1,14 +1,30 @@
+import { useParams } from 'react-router'
 import '../style/style.css'
 import data from '../data.json'
 import Carrousel from '../components/Carrousel'
-// import InfoProperty from '../components/InfoProperty'
+import InfoProperty from '../components/InfoProperty'
+import Error from './Error'
 
 function Accommodation() {
+  const { id } = useParams()
+  const property = data.find((item) => item.id === id)
+
+  if (!property) {
+    return <Error />
+  }
   return (
     <div className="page">
-      {data.map((carrousel) => {
-        return <Carrousel key={carrousel.id} pictures={carrousel.pictures} />
-      })}
+      <Carrousel pictures={property.pictures} />
+      <InfoProperty
+        title={property.title}
+        location={property.location}
+        name={property.host.name}
+        picture={property.host.picture}
+        tags={property.tags}
+        rating={property.rating}
+        description={property.description}
+        equipments={property.equipments}
+      />
     </div>
   )
 }
