d66ac1ff743037ae9b946bdfbbba11b6315989f5
Nathan Rodriguez <nathan94_roro@hotmail.fr>
2024-11-28 23:41:08 -0500

    Changement du chemin pour l'image de la banner

--- src/Page/About.js
+++ src/Page/About.js
@@ -1,5 +1,6 @@
 import Collapse from '../components/Collapse'
 import Banner from '../components/Banner'
+import banner2 from '../assets/kalen-emsley.png'
 import '../style/style.css'
 import '../style/about.css'
 import about from '../about.json'
@@ -7,7 +8,7 @@ import about from '../about.json'
 function About() {
   return (
     <div className="page">
-      <Banner />
+      <Banner image={banner2} accueil={false} />
       <div className="container_collapse">
         {about.map((collapse) => {
           return (
--- src/Page/Home.js
+++ src/Page/Home.js
@@ -1,5 +1,6 @@
 import '../style/style.css'
 import '../style/home.css'
+import banner from '../assets/eric-muhr.png'
 import Banner from '../components/Banner'
 import Card from '../components/Card'
 import data from '../data.json'
@@ -7,7 +8,7 @@ import data from '../data.json'
 function Home() {
   return (
     <div className="page">
-      <Banner />
+      <Banner image={banner} accueil={true} />
       <div className="cards">
         {data.map((card) => {
           return (
