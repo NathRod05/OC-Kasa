5a402aeb788b25985632059e90a4233119135c72
Nathan Rodriguez <nathan94_roro@hotmail.fr>
2024-12-03 20:44:16 -0500

    Ajout animation Collapse

--- src/components/Collapse.js
+++ src/components/Collapse.js
@@ -1,15 +1,27 @@
-import React, { useState } from 'react'
+import React, { useState, useRef } from 'react'
 import '../style/collapse.css'
 
 function Collapse({ title, description }) {
   const [open, setOpen] = useState(false)
+  const descriptionRef = useRef()
+
   return (
     <div className="collapse">
       <button className="collapse__btn" onClick={() => setOpen(!open)}>
-        {title}{' '}
+        {title}
         <i className={`fa-solid fa-chevron-${open ? 'down' : 'up'}`}></i>
       </button>
-      {open ? <ul className="descritpion">{description}</ul> : ''}
+      <div
+        className="description-content"
+        ref={descriptionRef}
+        style={
+          open
+            ? { height: descriptionRef.current.scrollHeight + 'px' }
+            : { height: '0px' }
+        }
+      >
+        <div className="description">{description}</div>
+      </div>
     </div>
   )
 }
